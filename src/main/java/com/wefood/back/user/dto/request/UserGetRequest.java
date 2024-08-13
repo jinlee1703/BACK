4d48892e94f8de2babbc574246965db4100cca9b
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-14 08:23:34 +0900

    feat: User 생성,조회,로그인 dto 생성

---
+++ src/main/java/com/wefood/back/user/dto/request/UserGetRequest.java
@@ -0,0 +1,4 @@
+package com.wefood.back.user.dto.request;
+
+public record UserGetRequest(String phoneNumber,String password) {
+}
---
+++ src/main/java/com/wefood/back/user/dto/request/UserLoginRequest.java
@@ -0,0 +1,4 @@
+package com.wefood.back.user.dto.request;
+
+public record UserLoginRequest(String phoneNumber,String password) {
+}
