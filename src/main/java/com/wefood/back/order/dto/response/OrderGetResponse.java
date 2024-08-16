11056f9bb34bf6b5347a07d4c6aee435aadc2277
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-17 02:47:04 +0900

    feat: OrderResponse 가 order_id를 갖도록  #15

--- src/main/java/com/wefood/back/order/dto/response/OrderGetResponse.java
+++ src/main/java/com/wefood/back/order/dto/response/OrderGetResponse.java
@@ -3,6 +3,6 @@ package com.wefood.back.order.dto.response;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record OrderGetResponse(String orderStatusId, int totalCost, String invoiceNumber, String receiverPhoneNumber,
+public record OrderGetResponse(Long id,String orderStatusId, int totalCost, String invoiceNumber, String receiverPhoneNumber,
                                String receiverName, LocalDate deliveryDate, LocalDateTime meetingAt) {
 }
