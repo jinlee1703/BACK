ad31218b71552daf3e36216b09e7e41ad648bf13
minm063 <jangmin063@gmail.com>
2024-08-19 21:09:12 +0900

    refactor: 변경 반영

--- src/main/java/com/wefood/back/product/repository/ProductRepository.java
+++ src/main/java/com/wefood/back/product/repository/ProductRepository.java
@@ -21,21 +21,21 @@ import java.util.Optional;
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("select new com.wefood.back.product.dto.ProductDetailResponse(p.id, p.name, p.detail, p.price, f.id, f.name) from Product p inner join Farm f on f.id=p.farm.id where p.id=:productId")
+    @Query("select new com.wefood.back.product.dto.ProductDetailResponse(p.id, p.name, p.detail, p.price, f.id, f.name) from Product p inner join Farm f on f.id=p.farm.id where p.id=:productId and p.isStatus=true")
     Optional<ProductDetailResponse> findProductDetailByProductId(@Param("productId") Long productId);
 
-    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where pi.isThumbnail=true order by p.id desc limit 4")
+    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where pi.isThumbnail=true and p.isStatus=true order by p.id desc limit 4")
     List<ProductResponse> findTop4ByOrderByIdDesc();
 
-    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductCategory c on p.id=c.pk.productId inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where c.pk.categoryId=:categoryId and pi.isThumbnail=true")
+    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductCategory c on p.id=c.pk.productId inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where c.pk.categoryId=:categoryId and p.isStatus=true and pi.isThumbnail=true")
     Page<ProductResponse> findProductByCategoryId(@Param("categoryId") Long id, Pageable pageable);
 
-    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where pi.isThumbnail=true and p.name like %:search%")
+    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where pi.isThumbnail=true and p.isStatus=true and p.name like %:search%")
     Page<ProductResponse> findByNameLike(@Param("search") String search, Pageable pageable);
 
-    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id inner join ProductTag pt on pt.pk.productId=p.id inner join Tag t on t.id=pt.pk.tagId where pi.isThumbnail=true and t.name like :search")
+    @Query("select new com.wefood.back.product.dto.ProductResponse(p.id, p.name, p.price, i.name) from Product p inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id inner join ProductTag pt on pt.pk.productId=p.id inner join Tag t on t.id=pt.pk.tagId where pi.isThumbnail=true and p.isStatus=true and t.name like :search")
     Page<ProductResponse> findByTag(@Param("search") String search, Pageable pageable);
 
-    @Query("select new com.wefood.back.order.dto.CartProductResponse(p.id, p.name, p.price, i.name, c.quantity) from Product p inner join Cart c on c.pk.productId=p.id inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where c.pk.userId=:userId and pi.isThumbnail=true")
+    @Query("select new com.wefood.back.order.dto.CartProductResponse(p.id, p.name, p.price, i.name, c.quantity) from Product p inner join Cart c on c.pk.productId=p.id inner join ProductImage pi on pi.pk.productId=p.id inner join Image i on pi.pk.imageId=i.id where c.pk.userId=:userId and pi.isThumbnail=true and p.isStatus=true")
     List<CartProductResponse> findCartProductByUserId(@Param("userId") Long userId);
 }
