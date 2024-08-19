a990057077dfeb2e661d22f42259404214a3180a
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-19 12:19:33 +0900

    feat: review api #16

--- src/main/java/com/wefood/back/order/service/OrderService.java
+++ src/main/java/com/wefood/back/order/service/OrderService.java
@@ -1,18 +1,18 @@
 package com.wefood.back.order.service;
 
 import com.wefood.back.order.dto.CartProductResponse;
-import com.wefood.back.order.dto.request.BasketOrderRequest;
-import com.wefood.back.order.dto.request.CartRequest;
-import com.wefood.back.order.dto.request.DirectOrderCreateRequest;
-import com.wefood.back.order.dto.request.OrderCreateRequest;
+import com.wefood.back.order.dto.request.*;
 import com.wefood.back.order.dto.response.OrderDetailGetResponse;
 import com.wefood.back.order.dto.response.OrderGetResponse;
+import com.wefood.back.order.dto.response.ReviewGetResponse;
 import com.wefood.back.order.entity.Order;
 import com.wefood.back.order.entity.OrderDetail;
 import com.wefood.back.order.entity.OrderStatus;
+import com.wefood.back.order.entity.Review;
 import com.wefood.back.order.repository.OrderDetailRepository;
 import com.wefood.back.order.repository.OrderRepository;
 import com.wefood.back.order.repository.OrderStatusRepository;
+import com.wefood.back.order.repository.ReviewRepository;
 import com.wefood.back.product.entity.Product;
 import com.wefood.back.product.repository.ProductRepository;
 import com.wefood.back.user.entity.User;
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -36,6 +37,16 @@ public class OrderService {
     private final UserRepository userRepository;
 
     private final ProductRepository productRepository;
+
+    private final ReviewRepository reviewRepository;
+
+    public List<ReviewGetResponse> findAllReview(Long userId) {
+        return reviewRepository.findByUserId(userId);
+    }
+
+    public ReviewGetResponse findReview(Long id) {
+        return reviewRepository.queryById(id);
+    }
 
     public void createOrder(Long userId, DirectOrderCreateRequest orderCreateRequest) {
 
@@ -81,7 +92,7 @@ public class OrderService {
 
         Order createOrder = orderRepository.save(order);
         Product product = productRepository.findById(orderCreateRequest.productId()).get();
-        OrderDetail orderDetail = new OrderDetail(createOrder, product, orderCreateRequest.quantity(), orderCreateRequest.price());
+        OrderDetail orderDetail = new OrderDetail(createOrder, product, null, orderCreateRequest.quantity(), orderCreateRequest.price());
         orderDetailRepository.save(orderDetail);
     }
 
@@ -138,7 +149,7 @@ public class OrderService {
             for (int j = 0; j < cartRequest.products().size(); j++) {
                 CartProductResponse cartProductResponse = cartRequest.products().get(j);
                 Product product = productRepository.findById(cartProductResponse.getId()).get();
-                OrderDetail orderDetail = new OrderDetail(createOrder, product, cartProductResponse.getQuantity(), cartProductResponse.getPrice());
+                OrderDetail orderDetail = new OrderDetail(createOrder, product, null, cartProductResponse.getQuantity(), cartProductResponse.getPrice());
                 orderDetailRepository.save(orderDetail);
             }
         }
@@ -151,6 +162,16 @@ public class OrderService {
 
     public List<OrderDetailGetResponse> findOrder(Long orderId) {
         return orderDetailRepository.findByOrderId(orderId);
+    }
+
+    public void createReview(ReviewCreateRequest createRequest, Long orderDetailId) {
+
+        OrderDetail orderDetail = orderDetailRepository.queryById(orderDetailId);
+        Product product = productRepository.findById(orderDetail.getProduct().getId()).get();
+        User user = userRepository.findById(orderDetail.getOrder().getUser().getId()).get();
+        Review resultReview = reviewRepository.save(new Review(orderDetail, product, user, createRequest.title(), createRequest.content(), LocalDateTime.now(), createRequest.score()));
+        orderDetail.setReview(resultReview);
+        orderDetailRepository.save(orderDetail);
     }
 
 }
