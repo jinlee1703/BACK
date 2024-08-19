e0d51021a797dc36631216e186c01c9a50cf3c44
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-19 12:18:55 +0900

    feat: review dto및 repo #16

---
+++ src/main/java/com/wefood/back/order/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.wefood.back.order.repository;
+
+import com.wefood.back.order.dto.response.ReviewGetResponse;
+import com.wefood.back.order.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<ReviewGetResponse> findByUserId(Long userId);
+
+    ReviewGetResponse queryById(Long id);
+
+}
